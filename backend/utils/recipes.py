9a52128d6891ec076bd09f74cd16221637cba539
Nguyễn Công Trình <nctrinh2705@gmail.com>
2025-08-16 17:02:47 +0700

    refactor: remove unnecessary blank line at the start of get_sufficient_recipes function

--- backend/utils/recipes.py
+++ backend/utils/recipes.py
@@ -1,4 +1,3 @@
-
 def get_sufficient_recipes(user_ingredients: list, recipes_data: dict, sort_by: str, sort_order: str, user_allergies: list = []):
     """
     Get all recipes that have all user's ingredients
